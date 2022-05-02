2c3f197b23014cd46069b7616808f9cd59e4acf4
BuildTools <unconfigured@null.spigotmc.org>
2022-05-02 20:21:12 +0800

    Add MOTD

--- src/main/java/p1xel/antijoin/Listener.java
+++ src/main/java/p1xel/antijoin/Listener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.server.ServerListPingEvent;
 
 public class Listener implements org.bukkit.event.Listener {
 
@@ -27,6 +28,21 @@ public class Listener implements org.bukkit.event.Listener {
 
         e.setResult(PlayerLoginEvent.Result.KICK_OTHER);
         e.setKickMessage(Config.getMessage("messages.anti-join"));
+
+    }
+
+    @EventHandler
+    public void onWTF(ServerListPingEvent e) {
+
+        if (AntiJoin.isAntiJoin) {
+
+            if (Config.getBool("motd.enable")) {
+
+                e.setMotd(Config.getMessage("motd.line1") + "\n" + Config.getMessage("motd.line2"));
+
+            }
+
+        }
 
     }
 
